826d9d14cf471ddda27cb46196c0fa73352c7718
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-04-14 13:43:27 +0530

    feat: Hero section added

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Shop.css";
+import Hero from "../../components/hero/Hero";
 
 const Shop = () => {
-  return <div>Shop</div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 };
 
 export default Shop;
