834013a9f14de918decbad5a84c058602470a230
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-04-14 14:56:32 +0530

    feat: Offer component added

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./Shop.css";
 import Hero from "../../components/hero/Hero";
 import Popular from "../../components/popular/Popular";
+import Offer from "../../components/offer/Offer";
 
 const Shop = () => {
   return (
     <div>
       <Hero />
       <Popular />
+      <Offer />
     </div>
   );
 };
