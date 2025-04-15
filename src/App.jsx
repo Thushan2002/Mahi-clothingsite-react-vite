72bd48b9d818a802fab0e8672c6d6d702c49dc43
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-04-15 21:52:52 +0530

    feat: Footer component added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Shop from "./pages/shop/Shop";
 import Shopcategory from "./pages/shop/Shopcategory";
 import Product from "./pages/product/Product";
 import Loginsignup from "./pages/login-signup/Loginsignup";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<Loginsignup />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
