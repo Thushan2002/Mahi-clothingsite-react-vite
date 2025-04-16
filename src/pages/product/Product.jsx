4b2eea773a9270c2b72a6cbd0f8e0aef0ddc9ae5
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-04-16 21:36:58 +0530

    feat:Breadcrums created

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Product.css";
+import { ShopContext } from "../../context/ShopContext";
+import { useParams } from "react-router-dom";
+import Breadcrum from "../../components/breadcrums/Breadcrum";
 
 const Product = () => {
-  return <div>Product</div>;
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_product.find((e) => e.id === Number(productId));
+  return (
+    <div>
+      <Breadcrum product={product} />
+    </div>
+  );
 };
 
 export default Product;
