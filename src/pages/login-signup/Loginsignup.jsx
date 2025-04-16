db585535b0542f53b2085735f75402c25156d679
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-04-16 17:33:05 +0530

    feat:Log-in page created

--- src/pages/login-signup/Loginsignup.jsx
+++ src/pages/login-signup/Loginsignup.jsx
@@ -2,7 +2,26 @@ import React from "react";
 import "./Loginsignup.css";
 
 const Loginsignup = () => {
-  return <div>Loginsignup</div>;
+  return (
+    <div className="loginSignup">
+      <div className="loginSignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginSignup-fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="text" placeholder="Your Email" />
+          <input type="text" placeholder="Your Password" />
+        </div>
+        <button>Sign Up</button>
+        <p className="loginSignup-login">
+          Already have an account? <span>Log In</span>
+        </p>
+        <div className="loginSignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, I agree to the terms and conditions.</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Loginsignup;
