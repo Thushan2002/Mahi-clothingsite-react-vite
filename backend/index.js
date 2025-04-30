d4fa3b5e8278f0c05b832077d23fe8d30da0dbe7
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-05-01 00:03:38 +0530

    feat: user model added

--- backend/index.js
+++ backend/index.js
@@ -150,3 +150,63 @@ app.post('/removeproduct', async (req, res) => {
     })
 })
 
+// schema for user model
+
+const Users = mongoose.model("Users", {
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    cartData: {
+        type: Object,
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+})
+//  creating endpoint to user registration
+
+app.post("/signup", async (req, res) => {
+    let check = await Users.findOne({ email: req.body.email })
+    if (check) {
+        return res.status(400).json({
+            success: true,
+            errors: "Exisiting email entered"
+        })
+    }
+    let cart = {}
+    for (let i = 0; i < 300; i++) {
+        cart[i = 0]
+    }
+    const user = new Users({
+        name: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        cartData: cart
+    })
+
+    await user.save()
+
+    const data = {
+        user: {
+            id: user.id
+        }
+    }
+
+    const token = jwt.sign(data, "secret_ecom")
+    res.json({
+        success: true,
+        token
+    })
+})
+
