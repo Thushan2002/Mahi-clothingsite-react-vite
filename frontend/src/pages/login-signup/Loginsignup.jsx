6ecc05ca98a5a134049412c6551ffe611b005a79
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-05-01 20:43:51 +0530

    feat: login and sign up added using state

--- frontend/src/pages/login-signup/Loginsignup.jsx
+++ frontend/src/pages/login-signup/Loginsignup.jsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Loginsignup.css";
 
 const Loginsignup = () => {
+  const [state, setState] = useState("Login");
+
   return (
     <div className="loginSignup">
       <div className="loginSignup-container">
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className="loginSignup-fields">
-          <input type="text" placeholder="Your Name" />
+          {state === "Sign Up" ? (
+            <input type="text" placeholder="Your Name" />
+          ) : null}
           <input type="text" placeholder="Your Email" />
           <input type="text" placeholder="Your Password" />
         </div>
-        <button>Sign Up</button>
-        <p className="loginSignup-login">
-          Already have an account? <span>Log In</span>
-        </p>
+        <button>{state === "Sign Up" ? "Sign Up" : "Log In"}</button>
+        {state === "Sign Up" ? (
+          <p className="loginSignup-login">
+            Already have an account?{" "}
+            <span onClick={() => setState("Login")}>Log In</span>
+          </p>
+        ) : (
+          <p className="loginSignup-login">
+            Don't have an account?{" "}
+            <span onClick={() => setState("Sign Up")}>Sign Up</span>
+          </p>
+        )}
+
         <div className="loginSignup-agree">
           <input type="checkbox" name="" id="" />
           <p>By continuing, I agree to the terms and conditions.</p>
